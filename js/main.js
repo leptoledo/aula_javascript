bd0c4caedecc3d8240740febd6f3efa36e0a2a34
Leandro Toledo <leandrotoledo@hotmail.com.br>
2020-11-28 20:14:48 +0000

    javascript-estudo

---
+++ js/main.js
@@ -0,0 +1,120 @@
+
+
+function clicou(){
+  document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+  //console.log(document.getElementById("agradecimento"));
+
+  //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+  window.open("https://globallabs.academy/");
+  //window.location.href = "https://globallabs.academy/";
+}
+
+function trocar(Elemento){
+  //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+  Elemento.innerHTML = "Obrigado por passar o mouse";
+  //alert("Trocar texto");
+}
+
+function voltar(Elemento){
+  //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+  Elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+  alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+  console.log(elemento.value)
+}
+
+/*
+  function soma(n1, n2){
+    return n1 + n2;
+  }
+*/
+
+/*
+  var validar = 0;
+  function validaIdade(idade){
+    if (idade >= 18){
+      validar = true
+    } else {
+      validar = false
+    }
+    return validar;
+  }
+
+  var idade = prompt("Qual sua idade");
+  validaIdade(idade)
+  console.log(validar);
+*/
+
+/*
+  var d = new Date();
+  alert(d.getDay());
+  alert(d.getHours());
+  alert(d.getMinutes());
+*/
+/*
+  var count
+  for(count=0; count <= 5; count++) {
+    alert(count);
+  };
+*/
+
+/*
+  var count = 0;
+  while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+  };
+*/
+
+/*
+  var idade = prompt("qual sua idade");
+  if (idade >= 18) {
+    alert("Maior de idade");
+  } else {
+    alert("Menor de idade");
+  };
+*/
+
+/*
+  var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+  console.log(frutas);
+  alert(frutas[1].nome);
+*/
+
+/*
+  var fruta = {nome: "maça", cor: "vermelha"}
+  console.log(fruta.nome);
+  alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+//console.log(lista.join(" | "));
+
+//console.log(lista.length);
+//console.log(lista.reverse);
+
+//var nome = "Leandro Toledo";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mudndo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.tolowerCase());
+//alert(frase.replace("Japão", "Brasil"));
